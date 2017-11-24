a7fedd0aafa591cd2dc5217ecd8e1d9fd1a3a5a3
Robert Arbon <robert.arbon@bristol.ac.uk>
2017-11-24 14:30:38 +0000

    uncommented client code

--- pyosc/client.py
+++ pyosc/client.py
@@ -3,25 +3,23 @@ import time
 import argparse
 import math
 
-# from pythonosc import osc_message_builder
-# from pythonosc import udp_client
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
 
 res_1 = np.load("../ala-dihed.npy")
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--ip", default="127.0.0.1",
-    #                     help="")
-    # parser.add_argument("--port", type=int, default=5005,
-    #                     help="")
-    # args = parser.parse_args()
-    #
-    # client = udp_client.SimpleUDPClient(args.ip, args.port)
-    #
-    # count = 0
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+                        help="")
+    parser.add_argument("--port", type=int, default=5005,
+                        help="")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+
     for x in res_1[:,1]:
-        print(x)
-        # client.send_message("/filter", x)
-        # time.sleep(1)
-        # count +=1
+        # print(x)
+        client.send_message("/filter", x)
+        time.sleep(1)
