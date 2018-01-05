0193336ab988fa05f822c7c210e23adf06328d73
Robert Arbon <robert.arbon@bristol.ac.uk>
2018-01-05 12:32:06 +0000

    Added tica trajectories to client. Added notebook for tica generation

--- pyosc/client.py
+++ pyosc/client.py
@@ -6,9 +6,9 @@ import math
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
-res_1 = np.load("/users/ajj/google drive/data/hmm_trajectories/traj-0.npy")
-res_2 = np.load("/users/ajj/google drive/data/hmm_trajectories/entropy-0.npy")
-
+phipsi = np.load("/users/ajj/google drive/data/hmm_trajectories/traj-0.npy")
+entropy = np.load("/users/ajj/google drive/data/hmm_trajectories/entropy-0.npy")
+tica = np.load("/users/ajj/google drive/data/tica_trajectories/traj-0.npy")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,12 +19,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     client = udp_client.SimpleUDPClient(args.ip, args.port)
-
-    for i, x in enumerate(res_1):
+    tica_idx = 0
+    for i, x in enumerate(phipsi):
         print(x)
         client.send_message("/state1", float(x[0]))
         client.send_message("/state2", float(x[1]))
         client.send_message("/state3", float(x[2]))
-        client.send_message("/entropy", float(res_2[i]))
-
+        client.send_message("/entropy", float(entropy[i]))
+        client.send_message("/tica", float(tica[i,tica_idx]))
         time.sleep(.05)
