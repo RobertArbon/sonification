3cbb96982ce34bb4f68358652b6c05887a1411d7
Robert Arbon <robert.arbon@bristol.ac.uk>
2018-01-19 15:41:26 +0000

    Added static properties message at start

--- pyosc/client.py
+++ pyosc/client.py
@@ -9,6 +9,11 @@ from pythonosc import udp_client
 phipsi = np.load("/users/ajj/google drive/data/hmm_trajectories/traj-0.npy")
 entropy = np.load("/users/ajj/google drive/data/hmm_trajectories/entropy-0.npy")
 tica = np.load("/users/ajj/google drive/data/tica_trajectories/traj-0.npy")
+properties = np.load("/users/ajj/google drive/data/static_properties_max_scale.npy")
+properties = np.load("/users/ajj/google drive/data/static_properties_01_scale.npy")
+
+
+#
 dtica = tica[:-1]-tica[1:]
 phipsi = phipsi[1:]
 entropy = entropy[1:]
@@ -22,6 +27,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+    for k, v in properties.items():
+        for i, x in enumerate(v):
+            client.send_message("/properties/{0}/state{1}".format(k,i), float(v))
+
     for i, x in enumerate(phipsi):
         print(x)
         client.send_message("/state1", float(x[0]))
